b9e4724bf55ae732909ce34d5c7244582a2948ca
Bala Murali <murali.marcelo12@gmail.com>
2025-04-21 19:27:21 -0700

    Fixed import of navbar

--- pitchconnect-frontend/src/Navbar.jsx
+++ pitchconnect-frontend/src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
+
 
 function Navbar() {
   const navigate = useNavigate();
