15bab2f4cfd5140942a4a754e19279bc4e4f6c0a
Bala Murali <murali.marcelo12@gmail.com>
2025-04-21 22:54:23 -0700

    changed upload pitch

--- pitchconnect-frontend/src/UploadPitch.jsx
+++ pitchconnect-frontend/src/UploadPitch.jsx
@@ -27,7 +27,7 @@ function UploadPitch() {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/pitches', {
+      const res = await fetch('https://itc505-team-india-website.onrender.com/api/pitches', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
